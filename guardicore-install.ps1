0809b16a12ab714e630898bfa8912a9821ade669
jrabinovitz-ideal <126910399+jrabinovitz-ideal@users.noreply.github.com>
2024-09-26 13:35:06 -0400

    Update guardicore-install.ps1

--- guardicore-install.ps1
+++ guardicore-install.ps1
@@ -3,7 +3,9 @@ $uri = "https://raw.githubusercontent.com/jrabinovitz-ideal/GuardiCore/refs/head
 $out = "C:\Temp\GC\windows_installer.exe"
 $Web.DownloadFile($uri,$out)
 
-$aggr_ip = "34.41.133.250"
+$aggr_ip = "35.239.155.15"
 $agent_pw = "XhakeIrC"
 
 $quiet_install = "C:\Windows\System32\cmd.exe /c C:\Temp\GC\windows_installer.exe /a $aggr_ip /p $agent_pw /q"
+
+invoke-expression -command $quiet_install
