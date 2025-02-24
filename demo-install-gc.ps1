a4520e296110b5a168b9152f7f40f839707a505e
jrabinovitz-ideal <126910399+jrabinovitz-ideal@users.noreply.github.com>
2025-02-24 12:38:08 -0500

    Update demo-install-gc.ps1

--- demo-install-gc.ps1
+++ demo-install-gc.ps1
@@ -1,9 +1,13 @@
 $aggr_agent = "https://aggr-customer-38001836-cloud.cloud.guardicore.com/windows_installer.exe"
 $aggr = "172.234.199.246"
+$output_agent = "C:\Windows\Temp\windows_installer.exe"
 $agent_pw = "4Bt1pePy2ighfQ7lQkF9"
+
 ##isntall
 function get-gcagent {
-	$dl_agent = Invoke-WebRequest -Uri $aggr_agent -OutFile C:\Windows\Temp\windows_installer.exe
+ 	$webClient = new-object System.Net.WebClient
+  	[System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }	
+  	 $webClient.DownloadFile($aggr_agent,$output_agent)
 }
 
 function upgrade-agent {
