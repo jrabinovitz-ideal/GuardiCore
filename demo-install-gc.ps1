f989f047e99c0405306dfe77ac870106cdedcd84
jrabinovitz-ideal <126910399+jrabinovitz-ideal@users.noreply.github.com>
2025-02-24 11:52:37 -0500

    Update demo-install-gc.ps1

--- demo-install-gc.ps1
+++ demo-install-gc.ps1
@@ -1,8 +1,10 @@
+$aggr_agent = "https://172.234.199.246/windows_installer.exe"
 $aggr = "172.234.199.246"
 $agent_pw = "4Bt1pePy2ighfQ7lQkF9"
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
 
 function get-gcagent {
-	$dl_agent = Invoke-WebRequest -Uri $aggr:443/windows_installer.exe -OutFile C:\Windows\Temp\windows_installer.exe
+	$dl_agent = Invoke-WebRequest -Uri $aggr_agent -OutFile C:\Windows\Temp\windows_installer.exe
 }
 
 function upgrade-agent {
